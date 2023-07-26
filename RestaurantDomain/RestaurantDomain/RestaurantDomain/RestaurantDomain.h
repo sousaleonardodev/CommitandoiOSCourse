be9deb42363e57de3537f1b67aab4f9eedaae4bf
Leonardo Sousa <sousaleonardo@outlook.com>
2023-07-26 14:31:20 -0300

    Create RestaurantDomain framework

---
+++ RestaurantDomain/RestaurantDomain/RestaurantDomain/RestaurantDomain.h
@@ -0,0 +1,13 @@
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RestaurantDomain.
+FOUNDATION_EXPORT double RestaurantDomainVersionNumber;
+
+//! Project version string for RestaurantDomain.
+FOUNDATION_EXPORT const unsigned char RestaurantDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestaurantDomain/PublicHeader.h>
+
+
