9e29ea88817fd153811a666871e8bc322933e203
Leonardo Sousa <sousaleonardo@outlook.com>
2023-07-26 14:53:16 -0300

    create restaurantLoader class

--- RestaurantDomain/RestaurantDomain/RestaurantDomain/RemoteRestaurantLoader.swift
+++ RestaurantDomain/RestaurantDomain/RestaurantDomain/RemoteRestaurantLoader.swift
@@ -2,24 +2,19 @@
 
 import Foundation
 
-final class NetworkClient {
-	static let shared: NetworkClient = NetworkClient()
-	
-	private init() {}
-	private(set) var urlRequest: URL?
-	
-	func request(from url: URL) {
-		urlRequest = url
-	}
+class NetworkClient {
+	static var shared: NetworkClient = NetworkClient()
+
+	func request(from url: URL) {}
 }
 
 final class RemoteRestaurantLoader {
 	let url: URL
-	
+
 	init(url: URL) {
 		self.url = url
 	}
-	
+
 	func load() {
 		NetworkClient.shared.request(from: url)
 	}
