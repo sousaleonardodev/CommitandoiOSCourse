d768451ce32f55a7d75041821bd96a49abbb7865
Leonardo Sousa <sousaleonardo@outlook.com>
2023-08-06 17:21:14 -0300

    create new tests to mapped invalid cases

--- RestaurantDomain/RestaurantDomain/RestaurantDomainTests/NetworkServiceTests.swift
+++ RestaurantDomain/RestaurantDomain/RestaurantDomainTests/NetworkServiceTests.swift
@@ -5,9 +5,9 @@ import XCTest
 @testable import RestaurantDomain
 
 final class NetworkServiceTests: XCTestCase {
-	func testRequestAndCreateDataTaskWithURLAndResume() throws {
+	func testRequestAndCreateDataTaskWithURLAndResume() {
 		let (sut, session, task) = makeSUT()
-		let url = try XCTUnwrap(URL(string: "https://comitando.com.br"))
+		let url = URL(string: "https://comitando.com.br")!
 
 		session.stub(url: url, task: task)
 
@@ -16,9 +16,9 @@ final class NetworkServiceTests: XCTestCase {
 		XCTAssertEqual(task.resumeCount, 1)
 	}
 
-	func testLoadRequestWithError() throws {
+	func testLoadRequestWithError() {
 		let (sut, session, task) = makeSUT()
-		let url = try XCTUnwrap(URL(string: "https://comitando.com.br"))
+		let url = URL(string: "https://comitando.com.br")!
 
 		let error = NSError(domain: "error", code: -1)
 		session.stub(url: url, task: task, error: error)
@@ -37,9 +37,9 @@ final class NetworkServiceTests: XCTestCase {
 		wait(for: [expectation])
 	}
 
-	func testLoadRequestWithSuccess() throws {
+	func testLoadRequestWithSuccess() {
 		let (sut, session, task) = makeSUT()
-		let url = try XCTUnwrap(URL(string: "https://comitando.com.br"))
+		let url = URL(string: "https://comitando.com.br")!
 		let data = Data()
 		let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
 
@@ -61,6 +61,37 @@ final class NetworkServiceTests: XCTestCase {
 		wait(for: [expectation])
 	}
 
+	func testLoadRequestWithErrorForInvalidCases() {
+		let url = URL(string: "https://commitando.com.br")!
+		let data = Data()
+		let anyError = NSError(domain: "testing error", code: -1)
+		let httpResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+		let urlResponse = URLResponse(url: url, mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+
+		XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: nil, error: nil))
+		XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: urlResponse, error: nil))
+		XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: httpResponse, error: nil))
+
+		XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: nil, error: nil))
+		XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: nil, error: anyError))
+		XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: urlResponse, error: nil))
+
+		XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: urlResponse, error: anyError))
+		XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: httpResponse, error: anyError))
+
+		XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: urlResponse, error: anyError))
+		XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: httpResponse, error: anyError))
+		XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: nil, error: anyError))
+	}
+
+	func testLoadRequestWithSuccessForValidCases() {
+		let url = URL(string: "https://commitando.com.br")!
+		let data = Data()
+		let httpResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+
+		XCTAssertNotNil(resultSuccessForValidCases(data: data, response: httpResponse, error: nil))
+	}
+
 	private func makeSUT(
 		file: StaticString = #filePath,
 		line: UInt = #line
@@ -75,9 +106,9 @@ final class NetworkServiceTests: XCTestCase {
 		return (sut, session, task)
 	}
 
-	private func resultErrorForInvalidCase(
+	private func resultErrorForInvalidCases(
 		data: Data?,
-		response: HTTPURLResponse?,
+		response: URLResponse?,
 		error: Error?,
 		file: StaticString = #filePath,
 		line: UInt = #line
@@ -93,9 +124,27 @@ final class NetworkServiceTests: XCTestCase {
 		return nil
 	}
 
+	private func resultSuccessForValidCases(
+		data: Data?,
+		response: URLResponse?,
+		error: Error?,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) -> (data: Data, response: HTTPURLResponse)? {
+		let result: NetworkClient.NetworkResult? = assert(data: data, response: response, error: error)
+
+		switch result {
+		case let .success((data, response)):
+			return (data, response)
+		default:
+			XCTFail("Should receive success", file: file, line: line)
+		}
+		return nil
+	}
+
 	private func assert(
 		data: Data?,
-		response: HTTPURLResponse?,
+		response: URLResponse?,
 		error: Error?,
 		file: StaticString = #filePath,
 		line: UInt = #line
@@ -122,14 +171,14 @@ final class URLSessionSpy: URLSession {
 	struct Stub {
 		let task: URLSessionDataTask
 		let data: Data?
-		let response: HTTPURLResponse?
+		let response: URLResponse?
 		let error: Error?
 
 		init(
 			task: URLSessionDataTask,
 			error: Error? = nil,
 			data: Data? = nil,
-			response: HTTPURLResponse? = nil)
+			response: URLResponse? = nil)
 		{
 			self.task = task
 			self.error = error
@@ -157,7 +206,7 @@ final class URLSessionSpy: URLSession {
 		task: URLSessionDataTask,
 		error: Error? = nil,
 		data: Data? = nil,
-		response: HTTPURLResponse? = nil
+		response: URLResponse? = nil
 	) {
 		stubs[url] = Stub(task: task, error: error, data: data, response: response)
 	}
