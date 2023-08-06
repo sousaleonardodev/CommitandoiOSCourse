f7dd6893d242aa4061259e7b7bc28d74af387279
Leonardo Sousa <sousaleonardo@outlook.com>
2023-08-06 16:56:56 -0300

    create extension to XCTestCase

---
+++ RestaurantDomain/RestaurantDomain/RestaurantDomainTests/XCTestCase+Extensions.swift
@@ -0,0 +1,12 @@
+//
+
+import Foundation
+import XCTest
+
+extension XCTestCase {
+	func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+		addTeardownBlock { [weak instance] in
+			XCTAssertNil(instance, "The instance should be dealloced. Possible Memory leak.", file: file, line: line)
+		}
+	}
+}
