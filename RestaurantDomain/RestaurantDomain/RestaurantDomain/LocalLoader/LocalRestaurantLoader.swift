70104ab9a0acb4d9bda4c809bfd136efd118edfa
Leonardo Sousa <sousaleonardo@outlook.com>
2023-08-07 16:04:27 -0300

    create initial structure to LocalRestauranteLoader

--- RestaurantDomain/RestaurantDomain/RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
+++ RestaurantDomain/RestaurantDomain/RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
@@ -2,3 +2,28 @@
 
 import Foundation
 
+protocol CacheClient {
+	func save(_ restaurants: [RestaurantItem], timestamp: Date, completion: @escaping (Error?) -> Void)
+	func delete(completion: @escaping (Error?) -> Void)
+}
+
+final class LocalRestaurantLoader {
+	let cacheClient: CacheClient
+	let currentDate: () -> Date
+
+	init(cacheClient: CacheClient, currentDate: @escaping () -> Date) {
+		self.cacheClient = cacheClient
+		self.currentDate = currentDate
+	}
+
+	func save(_ restaurants: [RestaurantItem], completion: @escaping (Error?) -> Void) {
+		cacheClient.delete { [cacheClient, currentDate] error in
+			guard error == nil else {
+				completion(error)
+				return
+			}
+
+			cacheClient.save(restaurants, timestamp: currentDate(), completion: completion)
+		}
+	}
+}
