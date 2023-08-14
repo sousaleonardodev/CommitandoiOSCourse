92b2518bb12f90f78f123385474c475947632d13
Leonardo Sousa <sousaleonardo@outlook.com>
2023-08-13 22:45:50 -0300

    Refactor code

--- RestaurantDomain/RestaurantDomain/RestaurantDomainTests/LocalRestaurantLoader/LocalRestauranteLoaderCacheTests.swift
+++ RestaurantDomain/RestaurantDomain/RestaurantDomainTests/LocalRestaurantLoader/LocalRestauranteLoaderCacheTests.swift
@@ -63,15 +63,6 @@ private extension LocalRestauranteLoaderCacheTests {
 		return (sut, cacheClient, currentDate)
 	}
 
-	func restaurantList() -> [RestaurantItem] {
-		[
-			.init(id: UUID(), name: "name", location: "location", distance: 1.0, ratings: 5, parasols: 11),
-			.init(id: UUID(), name: "name1", location: "location1", distance: 10.2, ratings: 2, parasols: 10),
-			.init(id: UUID(), name: "name2", location: "location2", distance: 20.5, ratings: 3, parasols: 9),
-			.init(id: UUID(), name: "name3", location: "location3", distance: 100.8, ratings: 1, parasols: 5)
-		]
-	}
-
 	private func assert(
 		_ sut: LocalRestaurantLoader,
 		completion result: RestaurantLoader.RestaurantResult?,
--- RestaurantDomain/RestaurantDomain/RestaurantDomainTests/XCTestCase+Extensions.swift
+++ RestaurantDomain/RestaurantDomain/RestaurantDomainTests/XCTestCase+Extensions.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 import XCTest
+import RestaurantDomain
 
 extension XCTestCase {
 	func trackForMemoryLeaks(
@@ -12,5 +13,14 @@ extension XCTestCase {
 		addTeardownBlock { [weak instance] in
 			XCTAssertNil(instance, "The instance should be dealloced. Possible Memory leak.", file: file, line: line)
 		}
+	}
+
+	func restaurantList() -> [RestaurantItem] {
+		[
+			.init(id: UUID(), name: "name", location: "location", distance: 1.0, ratings: 5, parasols: 11),
+			.init(id: UUID(), name: "name1", location: "location1", distance: 10.2, ratings: 2, parasols: 10),
+			.init(id: UUID(), name: "name2", location: "location2", distance: 20.5, ratings: 3, parasols: 9),
+			.init(id: UUID(), name: "name3", location: "location3", distance: 100.8, ratings: 1, parasols: 5)
+		]
 	}
 }
