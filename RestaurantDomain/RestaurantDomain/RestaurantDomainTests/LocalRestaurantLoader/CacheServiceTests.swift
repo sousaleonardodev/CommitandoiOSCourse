3a964f0cc5406f79bce9351063121da6717ffbf7
Leonardo Sousa <sousaleonardo@outlook.com>
2023-08-20 18:59:05 -0300

    create delete cache tests

--- RestaurantDomain/RestaurantDomain/RestaurantDomainTests/LocalRestaurantLoader/CacheServiceTests.swift
+++ RestaurantDomain/RestaurantDomain/RestaurantDomainTests/LocalRestaurantLoader/CacheServiceTests.swift
@@ -5,6 +5,11 @@ import XCTest
 
 final class CacheServiceTests: XCTestCase {
 
+	override func setUp() {
+		super.setUp()
+		try? FileManager.default.removeItem(at: validManagerURL())
+	}
+
 	func testSaveValue() {
 		let sut = makeSUT()
 		let items = restaurantList()
@@ -19,7 +24,6 @@ final class CacheServiceTests: XCTestCase {
 		let sut = makeSUT()
 		let firstTimeItems = restaurantList()
 		let firstTimeTimestamp = Date()
-		let expectation = XCTestExpectation(description: "filemanager expectation")
 
 		insert(sut, items: firstTimeItems, timestamp: firstTimeTimestamp)
 
@@ -33,12 +37,48 @@ final class CacheServiceTests: XCTestCase {
 
 	func testSaveReturnErrorWithInvalidURL() {
 		let invalidURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-		let sut = CacheService(managerURL: invalidURL)
+		let sut = makeSUT(managerURL: invalidURL)
 		let items = restaurantList()
 
 		let returnedError = insert(sut, items: items, timestamp: Date())
 
 		XCTAssertNotNil(returnedError)
+	}
+
+	func testDeleteEmptyCache() {
+		let sut = makeSUT()
+
+		assert(sut, completion: .empty)
+
+		var returnedError: Error? = delete(sut)
+
+		XCTAssertNil(returnedError)
+		assert(sut, completion: .empty)
+	}
+
+	func testDeleteWithCachedValues() {
+		let sut = makeSUT()
+		let items = restaurantList()
+		let timestamp = Date()
+
+		insert(sut, items: items, timestamp: timestamp)
+		assert(sut, completion: .success(items: items, timestamp: timestamp))
+
+		delete(sut)
+
+		assert(sut, completion: .empty)
+	}
+
+	func testDeleteReturnedEmptyAfterInsertingNewDate() {
+		let sut = makeSUT()
+		let items = restaurantList()
+		let timestamp = Date()
+
+		insert(sut, items: items, timestamp: timestamp)
+
+		delete(sut)
+
+		assert(sut, completion: .empty)
 	}
 
 	private func assert(
@@ -67,17 +107,20 @@ final class CacheServiceTests: XCTestCase {
 		wait(for: [expectation], timeout: 0.5)
 	}
 
-	private func makeSUT() -> CacheClient {
+	private func validManagerURL() -> URL {
 		let path = type(of: self)
-		let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appending(path: "\(path)")
-		return CacheService(managerURL: url!)
+
+		return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appending(path: "\(path)")
+	}
+
+	private func makeSUT(managerURL: URL? = nil) -> CacheClient {
+		return CacheService(managerURL: managerURL ?? validManagerURL())
 	}
 
 	@discardableResult
 	private func insert(_ sut: CacheClient, items: [RestaurantItem], timestamp: Date) -> Error? {
 		let expectation = XCTestExpectation(description: "filemanager wait")
 		var returnedError: Error?
-
 
 		sut.save(items, timestamp: timestamp) { error in
 			returnedError = error
@@ -86,6 +129,20 @@ final class CacheServiceTests: XCTestCase {
 
 		wait(for: [expectation], timeout: 0.5)
 
+		return returnedError
+	}
+
+	@discardableResult
+	private func delete(_ sut: CacheClient) -> Error? {
+		let expectation = XCTestExpectation(description: "delete expectation")
+
+		var returnedError: Error?
+		sut.delete { error in
+			returnedError = error
+			expectation.fulfill()
+		}
+
+		wait(for: [expectation], timeout: 0.5)
 		return returnedError
 	}
 }
