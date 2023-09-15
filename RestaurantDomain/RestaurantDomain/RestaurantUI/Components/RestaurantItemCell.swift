f68afe69e7bccdd5db1a5122b07450e1255f18d0
Leonardo Sousa <sousaleonardo@outlook.com>
2023-09-14 22:53:06 -0300

    Create RestaurantItemCell view code elements

--- RestaurantDomain/RestaurantDomain/RestaurantUI/Components/RestaurantItemCell.swift
+++ RestaurantDomain/RestaurantDomain/RestaurantUI/Components/RestaurantItemCell.swift
@@ -2,12 +2,68 @@
 
 import UIKit
 
+public protocol ViewCodeHelper {
+	func buildHierarchy()
+	func setupConstraints()
+	func setupAdditionalConfiguration()
+	func setupView()
+}
+
+public extension ViewCodeHelper {
+	func setupView() {
+		buildHierarchy()
+		setupConstraints()
+		setupAdditionalConfiguration()
+	}
+
+	func setupAdditionalConfiguration() {}
+}
+
 final class RestaurantItemCell: UITableViewCell {
-	private(set) var title = UILabel()
-	private(set) var location = UILabel()
-	private(set) var distance = UILabel()
-	private(set) var parasols = UILabel()
-	private(set) var ratings: [UIImageView] = {
-		.init(repeating: UIImageView(), count: 5)
-	}()
+	private(set) lazy var hStack = setupStackView(axis: .horizontal, spacing: 16, aligment: .center)
+	private(set) lazy var vStack = setupStackView(axis: .vertical, spacing: 4, aligment: .leading)
+	private(set) lazy var hRatingStack = setupStackView(axis: .horizontal, spacing: 0, aligment: .fill)
+
+	private(set) lazy var mapImage = setupImage("map")
+	private(set) lazy var title = setupLabel(font: .preferredFont(forTextStyle: .title2))
+	private(set) lazy var location = setupLabel(font: .preferredFont(forTextStyle: .body))
+	private(set) lazy var distance = setupLabel(font: .preferredFont(forTextStyle: .body))
+	private(set) lazy var parasols = setupLabel(font: .preferredFont(forTextStyle: .body))
+	private(set) lazy var ratings: [UIImageView] = renderImageCollection()
+
+	private func setupLabel(font: UIFont, textColor: UIColor = .black) -> UILabel {
+		let label = UILabel()
+		label.font = font
+		label.translatesAutoresizingMaskIntoConstraints = false
+
+		return label
+	}
+
+	private func setupStackView(axis: NSLayoutConstraint.Axis, spacing: CGFloat, aligment: UIStackView.Alignment) -> UIStackView {
+		let stack = UIStackView()
+
+		stack.axis = axis
+		stack.spacing = spacing
+		stack.alignment = aligment
+		stack.translatesAutoresizingMaskIntoConstraints = false
+
+		return stack
+	}
+
+	private func setupImage(_ systemName: String) -> UIImageView {
+		let imageView = UIImageView()
+		imageView.image = UIImage(systemName: systemName)
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+
+		return imageView
+	}
+
+	private func renderImageCollection() -> [UIImageView] {
+		var images = [UIImageView]()
+
+		for _ in 1...5 {
+			images.append(setupImage("star"))
+		}
+		return images
+	}
 }
